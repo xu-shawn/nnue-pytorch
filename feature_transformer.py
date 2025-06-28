c905b788204e967e983d62aabe5ff8b343111edd
Shawn Xu <xu107288696@gmail.com>
2025-06-28 11:56:31 -0700

    backward only

--- feature_transformer.py
+++ feature_transformer.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 from torch import autograd
+import triton
+import triton.language as tl
 import cupy as cp
 import math
 
@@ -174,6 +176,124 @@ void feature_transformer_slice_forward(
             kernel, (num_threads,)
         )
     return _feature_transformer_slice_forward_kernel_cache[key]
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({"OUTPUT_BLOCK_SIZE": 64}),
+        triton.Config({"OUTPUT_BLOCK_SIZE": 128}),
+        triton.Config({"OUTPUT_BLOCK_SIZE": 256}),
+        triton.Config({"OUTPUT_BLOCK_SIZE": 512}),
+        triton.Config({"OUTPUT_BLOCK_SIZE": 1024}),
+    ],
+    key=["batch_size", "output_size"]
+)
+@triton.jit
+def _feature_transformer_slice_bias_backward_kernel(
+        bias_grad,
+        output_grad,
+        batch_size: tl.constexpr,
+        output_size: tl.constexpr,
+        OUTPUT_BLOCK_SIZE: tl.constexpr
+):
+    output_block_idx = tl.program_id(0)
+
+    output_offsets = OUTPUT_BLOCK_SIZE * output_block_idx + tl.arange(0, OUTPUT_BLOCK_SIZE)
+    output_mask = output_offsets < output_size
+
+    acc = tl.zeros((OUTPUT_BLOCK_SIZE,), dtype=tl.float32)
+
+    for k in range(batch_size):
+        output_grad_slice = output_grad + k * output_size
+        output_grad_values = tl.load(output_grad_slice + output_offsets, mask=output_mask, other=0.0)
+        acc += output_grad_values
+
+    bias_update_mask = output_mask & (acc != 0.0)
+    tl.atomic_add(bias_grad + output_offsets, acc, mask=bias_update_mask)
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({"OUTPUT_BLOCK_SIZE": 32}),
+        triton.Config({"OUTPUT_BLOCK_SIZE": 64}),
+        triton.Config({"OUTPUT_BLOCK_SIZE": 128}),
+        triton.Config({"OUTPUT_BLOCK_SIZE": 256}),
+    ],
+    key=["max_active_features", "output_size"]
+)
+@triton.jit
+def _feature_transformer_slice_weight_backward_kernel(
+        feature_indices,
+        feature_values,
+        weight_grad,
+        output_grad,
+        max_active_features: tl.constexpr,
+        output_size: tl.constexpr,
+        OUTPUT_BLOCK_SIZE: tl.constexpr
+):
+    batch_idx = tl.program_id(0)
+    output_block_idx = tl.program_id(1)
+
+    output_offsets = OUTPUT_BLOCK_SIZE * output_block_idx + tl.arange(0, OUTPUT_BLOCK_SIZE)
+    output_mask = output_offsets < output_size
+
+    feature_indices_slice = feature_indices + batch_idx * max_active_features
+    feature_values_slice = feature_values + batch_idx * max_active_features
+
+    output_grad_slice = output_grad + batch_idx * output_size
+    output_grad_values = tl.load(output_grad_slice + output_offsets, mask=output_mask, other=0.0)
+
+    past_active_features = False
+    for k in range(max_active_features):
+        if not past_active_features:
+            feature_idx = tl.load(feature_indices_slice + k)
+            if feature_idx == -1:
+                past_active_features = True
+            else:
+                curr_feature_values = tl.load(feature_values_slice + k)
+                curr_weight_grad_values = output_grad_values * curr_feature_values
+                curr_weight_update_mask = output_mask & (curr_weight_grad_values != 0)
+                tl.atomic_add(
+                    weight_grad + feature_idx * output_size + output_offsets,
+                    curr_weight_grad_values,
+                    mask=curr_weight_update_mask
+                )
+
+
+def feature_transformer_slice_backward(
+        feature_indices,
+        feature_values,
+        bias_grad,
+        weight_grad,
+        output_grad,
+        batch_size,
+        max_active_features,
+        output_size
+):
+    def grid_bias(meta):
+        return (triton.cdiv(output_size, meta['OUTPUT_BLOCK_SIZE']),)
+
+    _feature_transformer_slice_bias_backward_kernel[grid_bias](
+        bias_grad=bias_grad,
+        output_grad=output_grad,
+        batch_size=batch_size,
+        output_size=output_size
+    )
+
+    def grid_weights(meta):
+        return (
+            batch_size,
+            triton.cdiv(output_size, meta['OUTPUT_BLOCK_SIZE'])
+        )
+
+    _feature_transformer_slice_weight_backward_kernel[grid_weights](
+        feature_indices=feature_indices,
+        feature_values=feature_values,
+        weight_grad=weight_grad,
+        output_grad=output_grad,
+        max_active_features=max_active_features,
+        output_size=output_size
+    )
 
 
 _feature_transformer_slice_backward_kernel_cache = dict()
@@ -394,18 +514,15 @@ class FeatureTransformerSliceFunction(autograd.Function):
         )
         bias_grad = torch.zeros(output_size, dtype=torch.float32, device=device)
 
-        kernel = make_feature_transformer_slice_backward_kernel(
-            max_active_features, output_size
-        )
-        kernel(
-            grid=(batch_size,),
-            args=(
-                feature_indices.data_ptr(),
-                feature_values.data_ptr(),
-                weight_grad.data_ptr(),
-                bias_grad.data_ptr(),
-                grad_output.data_ptr(),
-            ),
+        feature_transformer_slice_backward(
+            feature_indices=feature_indices,
+            feature_values=feature_values,
+            weight_grad=weight_grad,
+            bias_grad=bias_grad,
+            output_grad=grad_output,
+            batch_size=batch_size,
+            max_active_features=max_active_features,
+            output_size=output_size,
         )
 
         return None, None, weight_grad, bias_grad
@@ -545,29 +662,25 @@ class DoubleFeatureTransformerSliceFunction(autograd.Function):
         )
         bias_grad = torch.zeros(output_size, dtype=torch.float32, device=device)
 
-        kernel = make_feature_transformer_slice_backward_kernel(
-            max_active_features, output_size
+        feature_transformer_slice_backward(
+            feature_indices=feature_indices_0,
+            feature_values=feature_values_0,
+            weight_grad=weight_grad,
+            bias_grad=bias_grad,
+            output_grad=grad_output_0,
+            batch_size=batch_size,
+            max_active_features=max_active_features,
+            output_size=output_size,
         )
-        kernel(
-            grid=(batch_size,),
-            args=(
-                feature_indices_0.data_ptr(),
-                feature_values_0.data_ptr(),
-                weight_grad.data_ptr(),
-                bias_grad.data_ptr(),
-                grad_output_0.data_ptr(),
-            ),
-        )
-
-        kernel(
-            grid=(batch_size,),
-            args=(
-                feature_indices_1.data_ptr(),
-                feature_values_1.data_ptr(),
-                weight_grad.data_ptr(),
-                bias_grad.data_ptr(),
-                grad_output_1.data_ptr(),
-            ),
+        feature_transformer_slice_backward(
+            feature_indices=feature_indices_1,
+            feature_values=feature_values_1,
+            weight_grad=weight_grad,
+            bias_grad=bias_grad,
+            output_grad=grad_output_1,
+            batch_size=batch_size,
+            max_active_features=max_active_features,
+            output_size=output_size,
         )
 
         return None, None, None, None, weight_grad, bias_grad
