68b56ad3bfa98a6433b3e37fd4b26ba9155fbf2c
sscg13 <iamdumbybear1@gmail.com>
2025-11-04 02:30:33 -0800

    minor fix

--- model/model.py
+++ model/model.py
@@ -127,8 +127,8 @@ class LayerStacks(nn.Module):
     def forward(self, x: Tensor, ls_indices: Tensor):
         l1c_ = self.l1(x, ls_indices)
         l1x_, l1x_out = l1c_.split(self.L2, dim=1)
-        # multiply sqr crelu result by (127/128) to match quantized version
-        l1x_ = torch.clamp(torch.cat([torch.pow(l1x_, 2.0) * (127/128), l1x_], dim=1), 0.0, 1.0)
+        # multiply sqr crelu result by (255/256) to match quantized version
+        l1x_ = torch.clamp(torch.cat([torch.pow(l1x_, 2.0) * (255/256), l1x_], dim=1), 0.0, 1.0)
 
         l2c_ = self.l2(l1x_, ls_indices)
         l2x_ = torch.clamp(l2c_, 0.0, 1.0)
